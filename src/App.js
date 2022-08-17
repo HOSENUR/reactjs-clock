f859e4ddf156896d24b163256ee9b7aab89ff572
xense <jabedzaman2004@gmail.com>
2022-08-17 20:49:42 +0530

    add: App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import './App.css';
+import Clock from './components/Clock';
+import Date from './components/Date';
+
+function App() {
+  return (
+    <>
+    <Clock></Clock>
+    <Date></Date>
+    </>
+
+  );
+}
+
+export default App;
